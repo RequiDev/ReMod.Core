4451ec86f5d671ebedd3f75a0712b5c2a9ded824
Requi <privat@requi-dev.de>
2021-10-31 20:29:58 +0100

    Removed unnecessary files and code.

--- Managers/UiManager.cs
+++ Managers/UiManager.cs
@@ -23,39 +23,8 @@ namespace ReMod.Core.Managers
 
             FixLaunchpadScrolling();
 
-            var resourceManager = ResourceManager.Instance;
             MainMenu = new ReMenuPage(menuName, true);
             ReTabButton.Create(menuName, $"Open the {menuName} menu.", menuName, menuSprite);
-
-            MainMenu.AddCategoryPage("Movement", sprite: resourceManager.GetSprite("running"));
-
-            var visualCatPage = MainMenu.AddCategoryPage("Visuals", sprite: resourceManager.GetSprite("eye"));
-
-            visualCatPage.AddCategory("ESP/Highlights");
-            visualCatPage.AddCategory("Flashlight");
-            visualCatPage.AddCategory("Wireframe");
-            visualCatPage.AddCategory("Bone ESP");
-            visualCatPage.AddCategory("Nametags");
-
-            MainMenu.AddMenuPage("Dynamic Bones", sprite: resourceManager.GetSprite("bone"));
-            MainMenu.AddMenuPage("Avatars", sprite: resourceManager.GetSprite("hanger"));
-
-            var utilCatPage = MainMenu.AddCategoryPage("Utility", sprite: resourceManager.GetSprite("tools"));
-
-            utilCatPage.AddCategory("Quality of Life");
-            utilCatPage.AddCategory("Local Clone");
-            utilCatPage.AddCategory("Objects");
-            utilCatPage.AddCategory("Spoofing");
-            utilCatPage.AddCategory("Protection");
-            utilCatPage.AddCategory("Emojis");
-            utilCatPage.AddCategory("Media Controls");
-            utilCatPage.AddCategory("Staff Alerts");
-            utilCatPage.AddCategory("Near Clipping Plane");
-            utilCatPage.AddCategory("Application");
-
-            MainMenu.AddMenuPage("Logging", sprite: resourceManager.GetSprite("log"));
-            MainMenu.AddMenuPage("FBT", sprite: resourceManager.GetSprite("arms-up"));
-            MainMenu.AddMenuPage("Hotkeys", sprite: resourceManager.GetSprite("keyboard"));
 
             TargetMenu = new ReMenuCategory($"{menuName}", ExtendedQuickMenu.Instance.field_Private_UIPage_1.transform.Find("ScrollRect").GetComponent<ScrollRect>().content);
         }
