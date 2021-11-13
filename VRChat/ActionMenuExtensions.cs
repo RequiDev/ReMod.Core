2502eac5a8b0bd4eedb94a1f3f202d874f2fbf49
Requi <privat@requi-dev.de>
2021-11-13 23:00:35 +0100

    Added action menu extensions.

---
+++ VRChat/ActionMenuExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReMod.Core.VRChat
+{
+    public static class ActionMenuExtensions
+    {
+        public static bool IsOpen(this ActionMenuDriver actionMenuDriver)
+        {
+            return actionMenuDriver.GetLeftOpener().IsOpen() || actionMenuDriver.GetRightOpener().IsOpen();
+        }
+
+        public static bool IsOpen(this ActionMenuOpener amo)
+        {
+            return amo.field_Private_Boolean_0;
+        }
+
+        public static ActionMenuOpener GetLeftOpener(this ActionMenuDriver actionMenuDriver)
+        {
+            if (actionMenuDriver.field_Public_ActionMenuOpener_0.field_Public_Hand_0 ==
+                ActionMenuOpener.Hand.Left)
+                return actionMenuDriver.field_Public_ActionMenuOpener_0;
+            return actionMenuDriver.field_Public_ActionMenuOpener_1;
+        }
+
+        public static ActionMenuOpener GetRightOpener(this ActionMenuDriver actionMenuDriver)
+        {
+            if (actionMenuDriver.field_Public_ActionMenuOpener_1.field_Public_Hand_0 ==
+                ActionMenuOpener.Hand.Right)
+                return actionMenuDriver.field_Public_ActionMenuOpener_1;
+            return actionMenuDriver.field_Public_ActionMenuOpener_0;
+        }
+    }
+}
