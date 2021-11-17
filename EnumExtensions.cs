20207a171991c9c60cacfb91aa69ebec253c0b47
Requi <privat@requi-dev.de>
2021-11-17 05:17:58 +0100

    Added enum extensions.

---
+++ EnumExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReMod.Core
+{
+    public static class EnumExtensions
+    {
+        public static int ToInt<T>(this T value) where T : Enum
+        {
+            return (int)(object)value;
+        }
+
+        public static bool HasFlag<T>(this T one, T other) where T : Enum
+        {
+            return (one.ToInt() & other.ToInt()) == other.ToInt();
+        }
+
+        public static T RemoveFlag<T>(this T one, T other) where T : Enum
+        {
+            return (T)Enum.ToObject(typeof(T), one.ToInt() & ~other.ToInt());
+        }
+
+        public static T AddFlag<T>(this T one, T other) where T : Enum
+        {
+            return (T)Enum.ToObject(typeof(T), one.ToInt() | other.ToInt());
+        }
+    }
+}
