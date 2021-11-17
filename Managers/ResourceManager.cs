5939b61ad172c9e752facc9b7c9c4b6ff98ad7ca
Requi <privat@requi-dev.de>
2021-11-17 06:00:26 +0100

    Made ResourceManager static instead of instanced so multiple mods can access it.

--- Managers/ResourceManager.cs
+++ Managers/ResourceManager.cs
@@ -6,63 +6,40 @@ using UnityEngine;
 
 namespace ReMod.Core.Managers
 {
-    public class ResourceManager
+    public static class ResourceManager
     {
-        private readonly Dictionary<string, Texture2D> _textures = new Dictionary<string, Texture2D>();
-        private readonly Dictionary<string, Sprite> _sprites = new Dictionary<string, Sprite>();
+        private static readonly Dictionary<string, Texture2D> Textures = new Dictionary<string, Texture2D>();
+        private static readonly Dictionary<string, Sprite> Sprites = new Dictionary<string, Sprite>();
 
-        private readonly Assembly _ourAssembly;
-
-        private readonly string _resourcePath;
-
-        public static ResourceManager Instance { get; private set; }
-        public ResourceManager(Assembly ourAssembly, string resourcePath)
+        public static Texture2D LoadTexture(string resourceName, byte[] bytes)
         {
-            if (Instance != null)
+            if (Textures.ContainsKey(resourceName))
             {
-                throw new Exception("ResourceManager already exists.");
+                throw new ArgumentException("Resource already exists", nameof(resourceName));
             }
-            Instance = this;
-
-            _ourAssembly = ourAssembly;
-            _resourcePath = resourcePath;
-        }
-
-        public Texture2D GetTexture(string resourceName)
-        {
-            if (_textures.ContainsKey(resourceName))
-            {
-                return _textures[resourceName];
-            }
-
-            var resourcePath = $"{_resourcePath}.{resourceName}.png";
-            var stream = _ourAssembly.GetManifestResourceStream(resourcePath);
-            if (stream == null)
-            {
-                throw new ArgumentException($"Resource \"{resourcePath}\" doesn't exist", nameof(resourceName));
-            }
-
-            using var ms = new MemoryStream();
-            stream.CopyTo(ms);
-
+            
             var texture = new Texture2D(1, 1);
-            ImageConversion.LoadImage(texture, ms.ToArray());
+            ImageConversion.LoadImage(texture, bytes);
             texture.hideFlags |= HideFlags.DontUnloadUnusedAsset;
             texture.wrapMode = TextureWrapMode.Clamp;
 
-            _textures.Add(resourceName, texture);
+            Textures.Add(resourceName, texture);
 
             return texture;
         }
 
-        public Sprite GetSprite(string resourceName)
+        public static Texture2D GetTexture(string resourceName)
         {
-            if (_sprites.ContainsKey(resourceName))
-            {
-                return _sprites[resourceName];
-            }
+            return Textures.ContainsKey(resourceName) ? Textures[resourceName] : null;
+        }
 
+        public static Sprite LoadSprite(string resourceName, byte[] bytes)
+        {
             var texture = GetTexture(resourceName);
+            if (texture == null)
+            {
+                texture = LoadTexture(resourceName, bytes);
+            }
 
             var rect = new Rect(0.0f, 0.0f, texture.width, texture.height);
             var pivot = new Vector2(0.5f, 0.5f);
@@ -70,9 +47,14 @@ namespace ReMod.Core.Managers
             var sprite = Sprite.CreateSprite_Injected(texture, ref rect, ref pivot, 100.0f, 0, SpriteMeshType.Tight, ref border, false);
             sprite.hideFlags |= HideFlags.DontUnloadUnusedAsset;
 
-            _sprites.Add(resourceName, sprite);
+            Sprites.Add(resourceName, sprite);
 
             return sprite;
+        }
+
+        public static Sprite GetSprite(string resourceName)
+        {
+            return Sprites.ContainsKey(resourceName) ? Sprites[resourceName] : null;
         }
     }
 }
