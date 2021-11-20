b7ea19feb790fec9fe770a442b115cd77d7b860b
Kiokuu <78288562+Kiokuu@users.noreply.github.com>
2021-11-20 02:38:47 +0000

    Rename + ShowConfirmDialogWithCancel.

--- VRChat/QuickMenuExtensions.cs
+++ VRChat/QuickMenuExtensions.cs
@@ -7,8 +7,7 @@ namespace ReMod.Core.VRChat
 {
     public static class QuickMenuExtensions
     {
-        public delegate void ShowConfirmDialogDelegate(UIMenu uiMenu, string title, string body, Il2CppSystem.Action yesAction, Il2CppSystem.Action noAction=null);
-
+        public delegate void ShowConfirmDialogDelegate(UIMenu uiMenu, string title, string body, Il2CppSystem.Action onYes, Il2CppSystem.Action onNo=null);
         private static ShowConfirmDialogDelegate _showConfirmDialogDelegate;
 
         private static ShowConfirmDialogDelegate ShowConfirmDialogFn
@@ -32,9 +31,38 @@ namespace ReMod.Core.VRChat
             }
         }
         
-        public static void ShowConfirmDialog(this UIMenu uiMenu, string title, string body, Action yesAction, Action noAction=null)
+        public delegate void ShowConfirmDialogWithCancelDelegate(UIMenu uiMenu, string title, string body, string yesLabel, string noLabel, string cancelLabel, Il2CppSystem.Action onYes, Il2CppSystem.Action onNo, Il2CppSystem.Action onCancel);
+        private static ShowConfirmDialogWithCancelDelegate _showConfirmDialogWithCancelDelegate;
+        
+        private static ShowConfirmDialogWithCancelDelegate ShowConfirmDialogWithCancelFn
         {
-            ShowConfirmDialogFn.Invoke(uiMenu, title, body, yesAction, noAction);
+            get
+            {
+                if (_showConfirmDialogWithCancelDelegate != null)
+                    return _showConfirmDialogWithCancelDelegate;
+
+                var showConfirmDialogWithCancelFn = typeof(UIMenu).GetMethods().FirstOrDefault(m =>
+                {
+                    if (!m.Name.Contains("Method_Public_Void_String_String_String_String_String_Action_Action_Action"))
+                        return false;
+                    
+                    return XrefUtils.CheckMethod(m, "ConfirmDialog");
+                });
+
+                _showConfirmDialogWithCancelDelegate = (ShowConfirmDialogWithCancelDelegate)Delegate.CreateDelegate(typeof(ShowConfirmDialogWithCancelDelegate), showConfirmDialogWithCancelFn);
+
+                return _showConfirmDialogWithCancelDelegate;
+            }
+        }
+        
+        public static void ShowConfirmDialog(this UIMenu uiMenu, string title, string body, Action onYes, Action onNo=null)
+        {
+            ShowConfirmDialogFn.Invoke(uiMenu, title, body, onYes, onNo);
+        }
+        
+        public static void ShowConfirmDialogWithCancel(this UIMenu uiMenu, string title, string body, string yesLabel, string noLabel, string cancelLabel, Action onYes, Action onNo, Action onCancel)
+        {
+            ShowConfirmDialogWithCancelFn.Invoke(uiMenu, title, body, yesLabel, noLabel, cancelLabel, onYes, onNo, onCancel);
         }
     }
 }
