f5680e0cb86280279175e707480d179a79cd19ce
Lily <admin@mintlily.lgbt>
2022-03-05 13:30:20 -0500

    Move Spacer to be used on ReMenuCategory

--- UI/QuickMenu/IButtonPage.cs
+++ UI/QuickMenu/IButtonPage.cs
@@ -6,6 +6,7 @@ namespace ReMod.Core.UI.QuickMenu
     public interface IButtonPage
     {
         ReMenuButton AddButton(string text, string tooltip, Action onClick, Sprite sprite = null);
+        ReMenuButton AddSpacer(Sprite sprite = null);
         ReMenuToggle AddToggle(string text, string tooltip, Action<bool> onToggle, bool defaultValue = false);
         ReMenuToggle AddToggle(string text, string tooltip, ConfigValue<bool> configValue);
         ReMenuPage AddMenuPage(string text, string tooltip = "", Sprite sprite = null);
