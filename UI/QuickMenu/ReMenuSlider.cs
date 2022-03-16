1dd983a6aa4974ccfcfca942b37057d0e0255e3a
Requi <privat@requi-dev.de>
2022-03-16 04:26:04 +0100

    Refactored ReMenuSlider.

--- UI/QuickMenu/ReMenuSlider.cs
+++ UI/QuickMenu/ReMenuSlider.cs
@@ -4,7 +4,7 @@ using ReMod.Core.VRChat;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using VRC.UI.Elements.Controls;
+using Object = UnityEngine.Object;
 
 namespace ReMod.Core.UI.QuickMenu
 {
@@ -12,22 +12,24 @@ namespace ReMod.Core.UI.QuickMenu
     {
         private readonly Slider _sliderComponent;
 
-        private float _valueHolder;
-
         public ReMenuSlider(string text, string tooltip, Action<float> onSlide, Transform parent, float defaultValue = 0, float minValue = 0, float maxValue = 10) : base(QuickMenuEx.SliderPrefab, parent, $"Slider_{text}")
         {
-            GameObject.DestroyImmediate(GameObject.GetComponent<UIInvisibleGraphic>()); // Fix for having clickable area overlap main quickmenu ui
+            Object.DestroyImmediate(GameObject.GetComponent<UIInvisibleGraphic>()); // Fix for having clickable area overlap main quickmenu ui
 
-            var name = GameObject.transform.GetChild(0).GetComponentInChildren<TextMeshProUGUI>();
+            var name = RectTransform.GetChild(0).GetComponentInChildren<TextMeshProUGUI>();
             name.text = text;
 
-            var value = GameObject.transform.GetChild(1).GetComponentInChildren<TextMeshProUGUI>();
-            value.text = defaultValue.ToString();  //This shit don't work
+            var value = RectTransform.GetChild(1).GetComponentInChildren<TextMeshProUGUI>();
+            value.text = defaultValue.ToString("F");  //This shit don't work
 
             _sliderComponent = GameObject.GetComponentInChildren<Slider>();
             _sliderComponent.onValueChanged = new Slider.SliderEvent();
             _sliderComponent.onValueChanged.AddListener(new Action<float>(onSlide));
-            _sliderComponent.onValueChanged.AddListener((Action<float>)delegate (float val) { value.text = val.ToString("F"); });
+            _sliderComponent.onValueChanged.AddListener(new Action<float>(val =>
+            {
+                value.text = val.ToString("F");
+            }));
+
             _sliderComponent.minValue = minValue;
             _sliderComponent.maxValue = maxValue;
             _sliderComponent.value = defaultValue;
@@ -44,7 +46,6 @@ namespace ReMod.Core.UI.QuickMenu
 
         public void Slide(float value, bool callback = true)
         {
-            _valueHolder = value;
             _sliderComponent.Set(value, callback);
         }
     }
